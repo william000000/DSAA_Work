926c7407afb8b6f1a02fbd8cfe58704238e5b1a9
William <sugiraw@gmail.com>
2021-04-23 15:41:18 +0200

    Implementing feedback

--- serializer.py
+++ serializer.py
@@ -1,63 +1,195 @@
+"""
+Serialize Student Data
+"""
+import sys
 import json
-import xml.etree.ElementTree as et
+import xml.etree.ElementTree as Et
+
 
 class Student:
+    """
+    Student class with the following properties
+    """
     def __init__(self, student_id, first_name, last_name, password):
         self.student_id = student_id
         self.first_name = first_name
         self.last_name = last_name
         self.password = password
+
     def serialize(self, serializer):
+        """
+        Initialize the student properties
+        :param serializer:
+        :return:
+        """
         serializer.start_object('student_id', self.student_id)
         serializer.add_property('first_name', self.first_name)
         serializer.add_property('last_name', self.last_name)
         serializer.add_property('password', self.password)
 
+
 class JsonSerializer:
+    """
+    Json Serializer
+    """
     def __init__(self):
         self._current_object = None
+
     def start_object(self, object_name, object_id):
+        """
+        Initiate an object
+        :param object_name:
+        :param object_id:
+        :return:
+        """
         self._current_object = {
             'id': object_id
         }
+
     def add_property(self, name, value):
+        """
+        Add property in the dictionary
+        :param name:
+        :param value:
+        :return:
+        """
         self._current_object[name] = value
 
     def to_str(self):
+        """
+        Return in the string
+        :return:
+        """
         return json.dumps(self._current_object, default=lambda o: o.__dict__)
 
+
 class XmlSerializer:
+    """
+    XML Serializer
+    """
     def __init__(self):
         self._element = None
 
     def start_object(self, object_name, object_id):
-        self._element = et.Element(object_name, attrib = {'id': object_id })
+        """
+        It initiates the id
+        :param object_name:
+        :param object_id:
+        :return:
+        """
+        self._element = Et.Element(object_name, attrib={'id': object_id})
+
     def add_property(self, name, value):
-        prop = et.SubElement(self._element, name)
+        """
+        It adds a value in the dictionary
+        :param name:
+        :param value:
+        :return:
+        """
+        prop = Et.SubElement(self._element, name)
         prop.text = value
+
     def to_str(self):
-        return et.tostring(self._element, encoding = 'unicode')
-    
+        """
+        Convert to String
+        :return:
+        """
+        return Et.tostring(self._element, encoding='unicode')
+
+
+class CsvSerializer:
+    """
+    CSV Serializer
+    """
+    def __init__(self):
+        self._current_object = {}
+
+    def start_object(self, object_name, object_id):
+        """
+        It initiates the id
+        :param object_name:
+        :param object_id:
+        :return:
+        """
+        self._current_object = {
+            'id': object_id
+        }
+
+    def add_property(self, name, value):
+        """
+        It adds a value in the dictionary
+        :param name:
+        :param value:
+        :return:
+        """
+        self._current_object[name] = value
+
+    def to_str(self):
+        """
+        Convert to String
+        :return:
+        """
+        data = self._current_object.values()
+        data = list(data)
+        user_id = data[0]
+        first_name = data[1]
+        last_name = data[2]
+        password = data[3]
+
+        line = f'{user_id},{first_name},{last_name},{password}'
+        return line
+
 
 class SerializerFactory:
+    """
+    Our Factory for serializer
+    """
     def __init__(self):
         self._creators = {}
-    def register_format(self, format, creator):
-        self._creators[format] = creator
-    def get_serializer(self, format):
-        creator = self._creators.get(format)
+
+    def register_format(self, format_input, creator):
+        """
+        Register a format
+        :param format_input:
+        :param creator:
+        :return:
+        """
+        self._creators[format_input] = creator
+
+    def get_serializer(self, format_input):
+        """
+        Get a format for serializer
+        :param format_input:
+        :return:
+        """
+        creator = self._creators.get(format_input)
         if not creator:
-            raise ValueError("Invalid format, Please try again later!")
+            print("Invalid format, Please try again later!")
+            sys.exit(0)
         return creator()
 
 
 factory = SerializerFactory()
 factory.register_format('JSON', JsonSerializer)
 factory.register_format('XML', XmlSerializer)
+factory.register_format('CSV', CsvSerializer)
 
 
 class ObjectSerializer:
-    def serialize(self, serializable, format):
-        serializer = factory.get_serializer(format)
+    """
+    Object Serializer
+    """
+    def __init__(self):
+        self._obj = {}
+
+    @staticmethod
+    def serialize(serializable, format_input):
+        """
+        Make use of serializers
+        :param serializable:
+        :param format_input:
+        :return:
+        """
+        serializer = factory.get_serializer(format_input)
         serializable.serialize(serializer)
         return serializer.to_str()
--- student_controller.py
+++ student_controller.py
@@ -1,70 +1,119 @@
+"""
+Student Controller
+"""
 import re
-import socket
-import json
 
-HOST = '127.0.0.1'
-PORT = 65432
-DATA_SEPARATOR=':'
-LINE_SEPARATOR=','
-PASSWORD_PATTERN = '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})'
-students_json = []
+VALUE_SEPARATOR = ':'
+PART_SEPARATOR = ','
+RESERVED_CHARACTER = ("=", "!", "+")
+MIN_PASSWORD_LENGTH = 8
 
 
 class StudentOperation:
+    """
+    Student Operation Class
+    """
     def __init__(self):
         self.students = {}
 
     def load_students(self, file_name):
+        """
+        Load Student data
+        :param file_name:
+        :return:
+        """
         try:
-            with open(file_name, 'r') as s:
-                datafile = s.readlines()
+            with open(file_name, 'r') as stud_file:
+                datafile = stud_file.readlines()
 
             for line in datafile:
-                user_id = line.strip().split(LINE_SEPARATOR)[0]
-                user_id = user_id.strip().split(DATA_SEPARATOR)[1].replace('"', '')
+                user_id = line.strip().split(PART_SEPARATOR)[0]
+                user_id = user_id.strip().split(VALUE_SEPARATOR)[1].replace('"', '')
                 line = line.strip().replace("\n", "")
-                self.students.update({ user_id: line })
+                self.students.update({user_id: line})
 
         except FileNotFoundError:
             pass
         return self.students
 
-
-    def is_valid_id(self, user_id : str) -> bool:
+    def is_valid_id(self, user_id: str) -> bool:
+        """
+        Check whether user_id is valid or not
+        :param user_id:
+        :return:
+        """
         if self.students.get(user_id):
             return True
         return False
 
     def get_user(self, user_id):
+        """
+        Get Student Info
+        :param user_id:
+        :return:
+        """
         return self.students.get(user_id)
 
+    @staticmethod
+    def is_password_valid(password: str) -> bool:
+        """
+        Check whether password is valid or not
+        :param password:
+        :return:
+        """
+        password_length = len(password)
+        counter = 0
+        check_character = 0
+        regex = '^[0-9A-Za-z]+$'
 
-    def is_password_valid(self, password: str) -> bool:
-        is_matched = re.search(PASSWORD_PATTERN, password)
-
-        if not is_matched:
+        if password_length < MIN_PASSWORD_LENGTH:
             return False
-        return True
-        
-    def is_data_exist(self, file_name: str, user_id: str) -> bool:
+        for char in password:
+            if char in RESERVED_CHARACTER:
+                return False
+            if char.isalnum() or char == ' ':
+                counter += 1
+            elif not char.isalnum() and char != ' ':
+                check_character += 1
+                password = password.replace(char, '')
+            password = password.replace(' ', '')
+        if (counter + check_character) != password_length or (check_character == 0 or counter == 0):
+            return False
+        if re.search(regex, password):
+            return True
+        return False
+
+    @staticmethod
+    def is_data_exist(file_name: str, user_id: str) -> bool:
+        """
+        Check whether Student is already saved or not
+        :param file_name:
+        :param user_id:
+        :return:
+        """
         try:
-            with open(file_name, 'r') as f:
-                for data in f:
+            with open(file_name, 'r') as stud_file:
+                for data in stud_file:
                     if re.search(user_id, data):
                         return True
         except FileNotFoundError:
-            pass 
+            pass
         return False
 
     def add_to_final_list(self, file_name: str, user_id: str, data: str) -> bool:
+        """
+        Save a student in the specified file
+        :param file_name:
+        :param user_id:
+        :param data:
+        :return:
+        """
         if self.is_data_exist(file_name, user_id):
             return print("The User has already been updated in final_student.txt!")
-            
         try:
             with open(file_name, 'a') as user_file:
                 user_file.write(f'{data}')
 
         except FileNotFoundError:
-            pass 
+            pass
         return True
-
